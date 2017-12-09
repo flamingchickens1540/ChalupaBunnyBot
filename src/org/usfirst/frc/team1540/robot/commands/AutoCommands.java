08545fc38d92c69a30e6b5dcaace4275eee16d37
Nate Sales <nate.spy6@yahoo.com>
2017-12-09 15:04:41 -0800

    Fixed bugs and finalzied auto

--- src/org/usfirst/frc/team1540/robot/commands/AutoCommands.java
+++ src/org/usfirst/frc/team1540/robot/commands/AutoCommands.java
@@ -3,10 +3,27 @@ package org.usfirst.frc.team1540.robot.commands;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class AutoCommands extends CommandGroup {
+	String mode= "S";
 	
 	public AutoCommands() {
-		//addSequential(new StraightForTime(2));
-		//addSequential(new TurnWithGyro(90));
-		addSequential(new TurnForTime(10, "R"));
+		if(mode == "S") {
+			addSequential(new StraightForTime(4.7));
+		}else if(mode == "S+T") {
+			addSequential(new StraightForTime(4.7));
+			addSequential(new TurnForTime(0.5, "L"));
+		}else if(mode == "S+T+S") {
+			addSequential(new StraightForTime(4.7));
+			addSequential(new TurnForTime(0.5, "L"));
+			addSequential(new StraightForTime(2));
+		}else if(mode == "mess_everyone_up") {
+			addSequential(new StraightForTime(4.7));
+			addSequential(new TurnForTime(0.5, "L"));
+			addSequential(new StraightForTime(2));
+			addSequential(new TurnForTime(2, "L"));
+			addSequential(new StraightForTime(2));
+			
+		}else {
+			addSequential(new StraightForTime(4.7));
+		}
 	}	
 }
